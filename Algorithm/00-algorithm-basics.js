2e82011ab759755d77d1853a6d8b1b189c807512
arifulsajib <allaboutsajib@gmail.com>
2023-03-20 00:56:01 +0600

    algorithm basics

---
+++ Algorithm/00-algorithm-basics.js
@@ -0,0 +1,35 @@
+/* 
+Algorithm: 
+----------
+An Algorithm is a set of well-defined instructions to solve a paticular problem.
+
+example: sum of 2 numbers
+input- a, b
+add number using + and return value
+Output sum of a and b
+
+Characteristics:
+----------------
+1. Well defined inputs and outputs:
+in our example input is a, b and output is sum of a and b. no confusion about that.
+
+2. Each step should be clear and unambigous:
+in our example there are steps to solve the problem add number using + and return value. the step is very clear.
+
+3. Language independent:
+it should be written in a way that can be implemented by any programming language.
+
+
+Why Algorithms?
+---------------
+As a developer, you are going to come across problems that you need to solve. 
+learning algorithms translate to learning defferent techniques to efficiently solve those poroblems.
+
+Note:
+-----
+One problem can be solved in many ways using different algorithms.
+Every algorithm comes with its own tradeoffs when it comes to performance.
+
+
+
+*/
---
+++ Algorithm/01-algorithm-analysis-time-space.js
@@ -0,0 +1,15 @@
+/* 
+Algorithm analysis: 
+------------------
+There are multiple ways to solve a problem.
+Ex: there are multiple ways to sort a list of numbers
+How do we analyse which one of them is the most efficient algorithm?
+
+Generally, when we talk about performance, we use an absolute measure. if i can run 100 meters in 12 seconds, I am faster than someone who takes 15 seconds. But analysing algrorithms is a bit different.
+
+The absolute running time of an algorithm cannot be predicted since it depends on a number of factors.
+
+
+
+
+*/
