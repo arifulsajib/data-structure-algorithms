435e757951d40aff9bbf611c6e5a4e39a50eec21
arifulsajib <allaboutsajib@gmail.com>
2023-03-20 02:43:44 +0600

    Big O object array

---
+++ Algorithm/03-Big-O-Object-Array.js
@@ -0,0 +1,26 @@
+/* 
+Big-O Object:
+An object is a collection of key value pairs.
+Insert ---- O(1) Constant
+Remove ---- O(1) Constant
+Access ---- O(1) Constant
+Search ---- O(n) linear
+Obejct.keys    ---- O(n) linear
+Obejct.values  ---- O(n) linear
+Obejct.entries ---- O(n) linear
+
+
+Big-O Array:
+An array is a collection values.
+Insert/ Remove End ---- O(1) Constant
+Insert/ Remove Begining ---- O(n) linear
+Access --- O(n) Constant
+Search ---- O(n) linear
+
+push/pop ---- O(1) Constant
+shift/unshift/concat/slice/splice  ---- O(n) linear
+foreEach/map/filter/reduce ---- O(n) linear
+
+
+
+*/
