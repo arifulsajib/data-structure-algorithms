8d005a0951d4d08ae913ade3401dd5c6a75e7d4a
arifulsajib <allaboutsajib@gmail.com>
2023-04-05 00:07:05 +0600

    count occur array

---
+++ Data-Structures/Arrays/counting-the-occurances.js
@@ -0,0 +1,13 @@
+const countOccur = (arr) => {
+  const counts = {};
+
+  for (const num of arr) {
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+  }
+
+  return counts;
+};
+
+const arr = [5, 5, 5, 2, 2, 2, 2, 2, 9, 4];
+console.log(countOccur(arr));
+console.log(countOccur(arr)[5]);
