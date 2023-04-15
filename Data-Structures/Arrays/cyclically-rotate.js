20e69160dc395a3ca286f9cea1fdc116fe408fd5
arifulsajib <allaboutsajib@gmail.com>
2023-04-16 00:37:16 +0600

    rotate array

--- Data-Structures/Arrays/cyclically-rotate.js
+++ Data-Structures/Arrays/cyclically-rotate.js
@@ -1,12 +1,13 @@
-// JavaScript code for program
-// to cyclically rotate
-// an array by one
-function rotate(arr, n) {
+/* 
+JavaScript code for program to cyclically rotate an array by one 
+*/
+
+const rotate = (arr, n) => {
   var x = arr[n - 1],
     i;
   for (i = n - 1; i > 0; i--) arr[i] = arr[i - 1];
   arr[0] = x;
-}
+};
 
 var arr = [1, 2, 3, 4, 5];
 var n = arr.length;
