9985a7ccfc2b645a152e4448ed4673d2f4d735c7
arifulsajib <allaboutsajib@gmail.com>
2023-04-17 00:22:39 +0600

    rotate array

--- Data-Structures/Arrays/cyclically-rotate.js
+++ Data-Structures/Arrays/cyclically-rotate.js
@@ -3,21 +3,21 @@ JavaScript code for program to cyclically rotate an array by one
 */
 
 const rotate = (arr, n) => {
-  var x = arr[n - 1],
+  let x = arr[n - 1],
     i;
   for (i = n - 1; i > 0; i--) arr[i] = arr[i - 1];
   arr[0] = x;
 };
 
-var arr = [1, 2, 3, 4, 5];
-var n = arr.length;
+let arr = [1, 2, 3, 4, 5];
+let n = arr.length;
 
 document.write("Given array is <br>");
-for (var i = 0; i < n; i++) document.write(arr[i] + " ");
+for (let i = 0;; i < n; i++) document.write(arr[i] + " ");
 
 rotate(arr, n);
 
 document.write("<br>Rotated array is <br>");
-for (var i = 0; i < n; i++) document.write(arr[i] + " ");
+for (let i = 0; i < n; i++) document.write(arr[i] + " ");
 
 // Big o notation: O(n)
